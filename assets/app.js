d67e5a1ed86dbfdb15f5d7ed53c8296c2d71d544
Richard Song <rich@Richards-MBP.lan>
2023-11-15 15:50:53 -0800

    Activated getRestaurants()

--- assets/app.js
+++ assets/app.js
@@ -180,6 +180,6 @@ document.addEventListener('DOMContentLoaded', function() {
         latLongApi();
         getEvents();
         getHotels();
-        // getRestaurants();
+        getRestaurants();
     })
 })
