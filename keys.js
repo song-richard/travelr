44dd563fe40685168312e1a24b8b056aa38acd23
Richard Song <rich@Richards-MBP.lan>
2023-11-16 17:51:26 -0800

    Added API keys to keys.js

---
+++ keys.js
@@ -0,0 +1,4 @@
+const openWeatherApiKey = "8a9b986776d2999e3580193c86a5744c";
+const ticketMasterApiKey = "SGU2gyci9kgPhW35MA8NlejLs92Z4EM4";
+const restaurantApiKey = "b7633d815emsh3230d09d00cd30bp130f8fjsnd43c8fffa537"
+const hotelApiKey = "2533ec1a1amshf6c5a3c72a4243ap124946jsn68c2d2fed973"
